80987d1ed1a362f0688e978976f7d2b93b74f74a
NORIKIM <jinagold.k@gmail.com>
2024-08-30 11:10:06 +0900

    프로젝트 생성

---
+++ iDine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by 인터웍스_맥북 on 8/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by 인터웍스_맥북 on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
